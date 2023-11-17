228fe8f33bdc1a273bfe8dd71ae92504cff64f43
m2cci-garciacp <pablo.garcia-campos@etu-univ-grenoble-alpes.fr>
2023-11-17 13:49:50 +0100

    Serveur en parallel et modulaire. A tester avec le jeu

--- serveur.c
+++ serveur.c
@@ -70,14 +70,7 @@ void serveur_appli(char *service)
 	struct sockaddr_in *p_adr_socket, p_adr_client ;
 	int socket_id, socket_client ;
 	unsigned int nb_req_att;
-	char msg_in [READ_SIZE] ;
-	char msg_out [READ_SIZE] ;
-	int result_read ;
 	int pid ; 
-
-	int combinationSecrete[N_COLORS];
-	int playing = 1 ;
-	ResultTentative result_tentative ;
 	
 	// commun
 	socket_id = h_socket ( AF_INET , SOCK_STREAM ) ;
@@ -86,52 +79,74 @@ void serveur_appli(char *service)
 
 
 	h_listen ( socket_id, nb_req_att ) ;
-	// Serveur iteratif
-	while (true) {
+	// Serveur parallele
+	while (true) 
+	{
 		socket_client = h_accept ( socket_id , &p_adr_client ) ;
 		// connexion etablie
-		playing = 1 ;
-		initialisation ( combinationSecrete ) ;
-		strcpy( msg_out , printRegles() );
-		h_writes ( socket_client , msg_out , sizeof(msg_out) ) ;
-		// on commence a jouer
-		while ( playing ) {
-			strcpy( msg_in , "" ) ;
-			strcpy( msg_out , "" ) ;
-			result_read = h_reads ( socket_client , msg_in , READ_SIZE );   // ceci possera de problemes car il lit jusque atteint le READ_SIZE
-			 
-			result_tentative = tentative ( texteASeqInt( msg_in ) ) ;
-			//  si reussi -> fin
-			if ( result_tentative.trouve ) {
-				playing = 0 ;
-				strcpy( msg_out , fin () ) ;
-			} else {
-				strcpy ( msg_out , resultatATexte( result_tentative ) ) ; // convertir result_tentative a bonne format str pour client
-			}			
-			h_writes ( socket_client , msg_out , strlen(msg_out) );
-		}
-	}
-	// // Serveur : parallele
-	// while (true) {
-	// 	socket_client = h_accept ( socket_id , p_p_adr_client ) ; // int h_accept( int num_soc, struct sockaddr_in *p_adr_client );
-	//    	pid = fork ()
-	//    	if (pid == 0 ) {
-	//    		// fils
-	//    		h_close( socket_id ) ;
-	//    		answer = h_reads ( socket_client , msg , 100 );       // int h_reads ( int num_soc, char *tampon, int nb_octets );
-	//    		h_close ( socket_client ) ; 
-    // 		exit ( 0 ) ; 
-	//    	} else {
-	//    		// pere
-	//   		h_close ( socket_client ) ;                          // void h_close ( int socket ); 
-	//   	}                 
-	// }
-
-	
+		pid = fork () ;
+		if (pid == 0 )
+		{
+	   		// fils
+			partie1Joueur ( socket_id ) ;
+			h_close ( socket_client ) ; 
+    		exit ( 0 ) ; 
+		} 
+		else 
+		{
+			// pere
+			h_close ( socket_client ) ; 
+		}		
+	}	
 
 	// commun
-	h_close ( socket_id ) ;                                      // void h_close ( int socket );
+	h_close ( socket_id ) ;
 	
 }
 
 /******************************************************************************/	
+
+
+void partie1Joueur ( int socket_client ) 
+{
+	int combinationSecrete[N_COLORS];
+	int playing = 1 ;
+	char msg_in [READ_SIZE] ;
+	char msg_out [READ_SIZE] ;
+	int result_read ;
+
+	int playing = 1 ;
+	ResultTentative result_tentative ;
+
+	// init
+	calculerCombinaisonSecrete ( combinationSecrete ) ;
+	initialisation ( combinationSecrete ) ;
+	strcpy( msg_out , printRegles() ) ;
+	//**convertir msg_out format de reseau
+	h_writes ( socket_client , msg_out , sizeof(msg_out) ) ;
+
+	// on commence a jouer
+	while ( playing ) 
+	{
+		strcpy( msg_in , "" ) ;
+		strcpy( msg_out , "" ) ;
+		result_read = h_reads ( socket_client , msg_in , READ_SIZE ) ;   // ceci possera de problemes car il lit jusque atteint le READ_SIZE
+			
+		result_tentative = tentative ( texteASeqInt( msg_in ) ) ;
+		//  si reussi -> fin
+		if ( result_tentative.trouve ) 
+		{
+			playing = 0 ;
+			strcpy( msg_out , fin () ) ;
+			//**convertir msg_out format de reseau
+			h_writes ( socket_client , msg_out , strlen(msg_out) ) ;
+			break ;
+		} 
+		else 
+		{
+			strcpy ( msg_out , resultatATexte( result_tentative ) ) ; // convertir result_tentative a bonne format str pour client
+			//**convertir msg_out format de reseau		
+			h_writes ( socket_client , msg_out , strlen(msg_out) ) ;
+		}		
+	}
+}
