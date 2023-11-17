3db516c4f7a800dffebfc17ff19a198611248be9
m2cci-garciacp <pablo.garcia-campos@etu.univ-grenoble-alpes.fr>
2023-11-17 13:49:50 +0100

    Serveur en parallel et modulaire. A tester avec le jeu

--- mastermind.h
+++ mastermind.h
@@ -8,8 +8,9 @@ typedef struct {
 void initialisation(int combinaisonSecrete[L]) ;
 char * printRegles() ;
 ResultTentative tentative(int Seq[L]) ;
+char * fin() ;
+// faudra changer cettes fonctions, une fois on a decide le format d'envoi
 int * texteASeqInt(char *txt) ;
 char * resultatATexte(ResultTentative resultat) ;
-char * fin() ;
 
 
