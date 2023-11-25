82348947881641ca6bb94d66194ead47e381f41e
Pablo GC <pablo.garcia-campos@etu.univ-grenoble-alpes.fr>
2023-11-25 17:45:58 +0100

    avance avec les automates

--- testing/inputData/tester.sh
+++
@@ -1,9 +0,0 @@
-set -e
-# create data
-mkdir -p data
-./scripts/makeData.sh
-# run tests
-./scripts/automator.sh result.txt
-
-echo "Probablement sans erreurs d'input de difficulte (format fault)"
-echo ""
